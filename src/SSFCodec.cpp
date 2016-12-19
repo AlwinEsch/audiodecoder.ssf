a30ff4350c655417a8e7878faf59b12a2c0453da
Alwin Esch <alwin.esch@web.de>
2016-12-19 18:23:23 +0100

    add initial functions for instance handling (currently unused)

--- src/SSFCodec.cpp
+++ src/SSFCodec.cpp
@@ -108,6 +108,15 @@ void ADDON_Announce(const char *flag, const char *sender, const char *message, c
 {
 }
 
+ADDON_STATUS ADDON_CreateInstance(int instanceType, const char* instanceID, const void* instanceProps, void* instanceFunctions, void* kodiInstance, void** addonInstance)
+{
+  return ADDON_STATUS_UNKNOWN;
+}
+
+void ADDON_DestroyInstance(int instanceType, const char* instanceID, void* instance)
+{
+}
+
 struct sdsf_load_state
 {
   std::vector<uint8_t> state;
